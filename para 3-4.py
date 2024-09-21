2180fcbf8752b92914c98c8ad352e9eea7a79004
Laminate32 <enotik.cool18@gmail.com>
2024-09-21 14:47:40 +0300

    Create para 3-4.py

---
+++ para 3-4.py
@@ -0,0 +1,96 @@
+# Para 3
+# print("Hi my name is Matvii) - 1 argument.
+# print("Hi", "My", "Name", "Is", "Matvii) - 5 arguments.
+from random import random
+
+# f = форматований вивід
+# print(f"")
+
+# print(5 + 5) - int
+# print("5+5") - str
+# print(5.5) - float
+# print(5>2) - bool (true/false)
+
+# Data Types
+#namedt = content
+# age = 60 #int
+# name = "Bohdan" #str
+# height = 150.5 #float
+# year = 2024 #int
+# price = 199.9 #float
+#
+# a = 5
+# b = 10
+
+# var 1
+# print(age, name, height, year, price)
+
+# var 2
+# print("\nName:", name, "\nAge:", age, "\nHeight", height, "\nYear:", year, "\nPrice:", price)
+
+# var 3
+# print(f"\n{name} \n{age} \n{height} \n{year} \n{price}")
+
+# Independent work Para 3
+
+# car = "Audi"
+# color = "Black"
+# year = 2014
+# model = "A4"
+#
+# print(f"\nCar: {car} \ncolor: {color} \nyear = {year} \nmodel = {model}")
+#
+# car = "BMW"
+# model = "X5"
+# year = 2017
+# color = "Blue"
+#
+# print(f"\nCar: {car} \ncolor: {color} \nyear = {year} \nmodel = {model}")
+
+
+#Para 4
+
+#Operators
+
+
+a = 5
+b = 20
+c = a+b
+# print(c)
+
+# print(f"{a} + {b} = {c}")
+# print(f"{a} * {b} = {a*b}")
+
+#Input
+
+# print(input("name = "))
+# import random
+# import time
+# a = 1
+# Username = str(input("input ur name: "))
+# Password = str(input("Input ur pass: "))
+# captcha0 = random.randint(0, 10000)
+# captcha = print(int(random.random()))
+#
+# print("Hi,",Username)
+# time.sleep(2)
+# print("Ur pass:",Password)
+# time.sleep(2)
+# print("Also please pass captcha:")
+# print(captcha)
+# captcha_write = int(input("Captcha: "))
+# if captcha_write != captcha:
+#     print("Wrong Captcha!")
+# else:
+#     print("Captcha passed! Hello!")
+
+#Площа та периметр прямокутника
+
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# perimeter = 2*(a+b)
+# size = a*b
+# print("Perimeter is:", perimeter)
+# print("Size is:", size)
+
+
