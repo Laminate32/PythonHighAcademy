4c3ea765a4fbf8d155fa59d0e08dbc9c93f3782d
Laminate32 <enotik.cool18@gmail.com>
2024-10-12 16:40:22 +0300

    HW Uploaded

---
+++ HW4.py
@@ -0,0 +1,70 @@
+
+# TASK 1
+# import math
+# a = int(input("Введіть любе ціле число: "))
+# b = int(input("Введіть друге любе ціле число(якщо не потребує, то пишіть що завгодно):"))
+# c = input("Яку площу хочете?:")
+# r = int(input("Якщо це коло, то введіть радіус(см)(якщо ні, то пишіть що завгодно):"))
+# h = int(input("Якщо це трикутник, то введіть висоту(см)(якщо ні, то пишіть що завгодно):"))
+# if c == "Коло":
+#     print(f"Площа кола: {math.pi*(r*r)}")
+# if c == "Трикутник":
+#     print(f"Площа трикутника: {0.5*a*h}")
+# if c == "Прямокутник":
+#     print(f"Площа прямокутника: {a*b}")
+# if c == "Квадрат":
+#     print(f"Площа квадрата: {a*a}")
+
+
+# TASK 2
+# a = int(input("Input weekday(number (1-7)):"))
+# if a == 1:
+#     print("Your weekday is Monday!")
+# elif a == 2:
+#     print("Your weekday is Tuesday!")
+# elif a == 3:
+#     print("Your weekday is Wednesday!")
+# elif a == 4:
+#     print("Your weekday is Thursday!")
+# elif a == 5:
+#     print("Your weekday is Friday!")
+# elif a == 6:
+#     print("Your weekday is Saturday!")
+# elif a == 7:
+#     print("Your weekday is Sunday!")
+
+# TASK 3
+# a = int(input("Input month(number (1-7)):"))
+# if a == 1:
+#     print("Your month is January!")
+# elif a == 2:
+#     print("Your month is February!")
+# elif a == 3:
+#     print("Your month is March!")
+# elif a == 4:
+#     print("Your month is April!")
+# elif a == 5:
+#     print("Your month is May!")
+# elif a == 6:
+#     print("Your month is June!")
+# elif a == 7:
+#     print("Your month is July!")
+# elif a == 8:
+#     print("Your month is August!")
+# elif a == 9:
+#     print("Your month is September!")
+# elif a == 10:
+#     print("Your month is October!")
+# elif a == 11:
+#     print("Your month is November!")
+# elif a == 12:
+#     print("Your month is December!")
+
+# TASK 4
+# a = int(input("Input random number:"))
+# if a == 0:
+#     print("Number is Equal")
+# elif a > 0:
+#     print("Number is Positive")
+# elif a < 0:
+#     print("Number is Negative")
