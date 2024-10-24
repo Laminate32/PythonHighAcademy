f673d9d5d0f34204d87e365b36f1593c0fbdcab0
Laminate32 <enotik.cool18@gmail.com>
2024-10-25 00:06:10 +0300

    HW5 uploaded

---
+++ HW5.py
@@ -0,0 +1,39 @@
+#Task 1
+# amount_of_work = int(input("Input your work time(hours):"))
+# stavka = int(input("Input your rate:"))
+# zarplata = int(input("Input your basic zarplata(dollars):"))
+# if amount_of_work > 24 or amount_of_work < 0:
+#     print("Error.")
+# if amount_of_work > 8:
+#     zarplata = zarplata * 1.5
+# print(f"Your zarplata(dollars): {zarplata}")
+
+#Task 2
+# chyslo = int(input("Input your chyslo:"))
+# stepin = int(input("Input your stepin(0-7):"))
+# if stepin > 7 or stepin < 0:
+#     print("Error")
+# else:
+#     chyslo = chyslo ** stepin
+#     print(f"Your chyslo: {chyslo}")
+
+#Task 3
+# vartist_rozmovy = int(input("Input your vartist rozmovy:"))
+# Lifecell = 5
+# Kyivstar = 10
+# Vodafone = 20
+# if vartist_rozmovy == "Lifecell":
+#     vartist_rozmovy == vartist_rozmovy + Lifecell
+# if vartist_rozmovy == "Kyivstar":
+#     vartist_rozmovy == vartist_rozmovy + Kyivstar
+# if vartist_rozmovy == "Vodafone":
+#     vartist_rozmovy == vartist_rozmovy + Vodafone
+
+#Task 4
+# chyslo = int(input("Input your chyslo:"))
+# if chyslo % 3 == 0:
+#     print("Fizz")
+# if chyslo % 5 == 0:
+#     print("Buzz")
+# if chyslo < 1 or chyslo > 100:
+#     print("Error")
